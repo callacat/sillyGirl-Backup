96f9c3e57358d12748e7d8be781c33fca50a9989
892947707 <wang892947707@hotmail.com>
2023-02-25 22:43:51 +0800

    update

--- Operation.sh
+++
@@ -1,5 +0,0 @@
-#bin/bash
-v=`curl https://raw.githubusercontent.com/cdle/binary/main/compile_time.go --silent | tr -cd "[0-9]"`
-# echo $v
-creatDate=$((v/1000))
-echo $(date '+%Y-%m-%d' -d @$creatDate)
